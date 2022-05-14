9b2a72870394f28d7a479e61510d90aa8e6b7505
Dhanar Adi Dewandaru <dhanar.adi.dewandaru@gmail.com>
2022-05-14 16:59:41 +0700

    test(useetv): revise test case

--- test.py
+++ test.py
@@ -7,17 +7,20 @@ from api.useetv import UseeTvApi
 class UseeTvApiTests(unittest.TestCase):
     def test_get_channel_list(self):
         api = UseeTvApi()
-        self.assertTrue(len(api.get_channel_list()) > 0)
+        channel_list = api.get_channel_list()
+        #print(channel_list)
+        self.assertTrue("tvri" in channel_list)
 
     def test_get_url_channel_valid(self):
         api = UseeTvApi()
-        channel = api.get_channel_list()[0]
-        self.assertTrue(api.get_url(channel))
+        url = api.get_url("tvri")
+        #print(url)
+        self.assertTrue(url)
 
     def test_get_url_channel_invalid(self):
         api = UseeTvApi()
         with self.assertRaises(urllib.error.HTTPError):
-            api.get_url("null")
+            api.get_url("invalid")
 
 
 if __name__ == "__main__":
