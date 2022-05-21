4144f10ca986e6a5dd31982bb1fd855bc3d45e71
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2022-05-22 06:27:18 +0700

    feat: add provider tvri

---
+++ providers/tvri.py
@@ -0,0 +1,30 @@
+from http.cookiejar import CookieJar
+from urllib.request import build_opener, HTTPCookieProcessor
+
+
+class Provider:
+    def __init__(self):
+        self._opener = build_opener(HTTPCookieProcessor(CookieJar()))
+        self._channels = {
+            "tvriworld": "http://118.97.50.107/Content/HLS/Live/Channel(TVRI3)/index.m3u8",
+            "tvrijakarta": "http://118.97.50.107/Content/HLS/Live/Channel(TVRIDKI)/index.m3u8",
+            "tvrijawatengah": "http://118.97.50.107/Content/HLS/Live/Channel(TVRIJATENGSEMARANG)/index.m3u8",
+            "tvriyogyakarta": "http://118.97.50.107/Content/HLS/Live/Channel(TVRIYOGYAKARTA)/index.m3u8"
+        }
+
+    def get_channel_names(self):
+        return self._channels.keys()
+
+    def get_channel_m3u(self, channel_name):
+        if not channel_name in self._channels:
+            raise Exception("Wrong channel name")
+        base_url = self._channels[channel_name].rsplit("/", 1)[0]
+        m3u = self._opener.open(self._channels[channel_name]).read().decode(
+            "utf-8").splitlines()
+        for i in range(len(m3u)):
+            if not m3u[i].startswith("#"):
+                m3u[i] = base_url + "/" + m3u[i]    # Add base URL
+        return "\n".join(m3u)
+
+    def get_kodi_headers_suffix(self):
+        return ""
