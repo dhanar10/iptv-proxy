ae5581bb97dbb33e5599e721e4e7988879a8022e
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2022-06-19 05:36:15 +0700

    chore(useetv): refactor

--- providers/useetv.py
+++ providers/useetv.py
@@ -8,20 +8,22 @@ from time import time
 
 
 class Provider:
+    CHANNEL_NAME_OVERRIDES = {
+        "ruangtrampil": "useeinfo"
+    }
+    MPD_NS = "urn:mpeg:dash:schema:mpd:2011"
+    MPD_CHANNELS = [
+        "useeinfo",
+        "useephoto",
+        "tvri",
+        "beritasatu",
+        "pramborstv",
+        "indikids",
+    ]
+
     def __init__(self):
         self._opener = build_opener(HTTPCookieProcessor(CookieJar()))
         self._opener.addheaders = [('User-Agent', 'Mozilla')]
-        self._channel_name_override = {
-            "ruangtrampil": "useeinfo"
-        }
-        self._mpd_channels = [
-            "useeinfo",
-            "useephoto",
-            "tvri",
-            "beritasatu",
-            "pramborstv",
-            "indikids",
-        ]
         self._mpd_url_cache = {}
 
     def get_channel_names(self):
@@ -35,7 +37,7 @@ class Provider:
         )
         if not channel_names:
             raise Exception("Failed to get channel names")
-        for k, v in self._channel_name_override.items():
+        for k, v in self.CHANNEL_NAME_OVERRIDES.items():
             channel_names[channel_names.index(k)] = v
         # return list(set(channel_names) - set(self._mpd_channels))
         return channel_names
@@ -61,30 +63,31 @@ class Provider:
             m3u_query = False
             mpd_query = True
         if m3u_query:
-            playlist = self._opener.open(b64decode(m3u_query.group("value")).decode(
-                "utf-8")).read().decode("utf-8")
+            url = b64decode(m3u_query.group("value")).decode(
+                "utf-8")
+            playlist = self._opener.open(url).read().decode("utf-8")
         elif mpd_query:
             if not url:
                 url = mpd_query.group("value")
                 self._mpd_url_cache[channel_name] = (int(time()) + 300, url)
-            mpd_xml = self._opener.open(url).read()
-            mpd_xml_ns = "urn:mpeg:dash:schema:mpd:2011"
-            ET.register_namespace("", mpd_xml_ns)
-            mpd_root = ET.fromstring(mpd_xml)
             base_url = url.split("?")[0].rsplit("/", 1)[0] + "/"
-            for mpd_adaptationset in mpd_root.iterfind(f".//{{{mpd_xml_ns}}}AdaptationSet"):
-                 for mpd_representation in mpd_adaptationset.findall(f"{{{mpd_xml_ns}}}Representation"):
-                    if mpd_representation.get("mimeType") == "video/mp4" and (not mpd_representation.get("height") in ("480", "540")):    # FIXME
-                            mpd_adaptationset.remove(mpd_representation)
-            for mpd_segmenttemplate in mpd_root.iterfind(f".//{{{mpd_xml_ns}}}Representation[@mimeType='video/mp4']/{{{mpd_xml_ns}}}SegmentTemplate"):
-                mpd_segmenttemplate.set("initialization", base_url + "/" + mpd_segmenttemplate.get("initialization"))
-                mpd_segmenttemplate.set("media", f"{channel_name}/video/$Time$")
-            for mpd_segmenttemplate in mpd_root.iterfind(f".//{{{mpd_xml_ns}}}Representation[@mimeType='audio/mp4']/{{{mpd_xml_ns}}}SegmentTemplate"):
-                mpd_segmenttemplate.set("initialization", base_url + "/" + mpd_segmenttemplate.get("initialization"))
-                mpd_segmenttemplate.set("media", f"{channel_name}/audio/$Time$")
+            mpd_str = self._opener.open(url).read()
+            ET.register_namespace("", self.MPD_NS)
+            mpd_root = ET.fromstring(mpd_str)
+            self._limit_video_representation(mpd_root)
+            # FIXME pramborstv representation id
+            for mpd_segmenttemplate in mpd_root.iterfind(f".//{{{self.MPD_NS}}}Representation[@mimeType='video/mp4']/{{{self.MPD_NS}}}SegmentTemplate"):
+                mpd_segmenttemplate.set(
+                    "initialization", base_url + "/" + mpd_segmenttemplate.get("initialization"))
+                mpd_segmenttemplate.set(
+                    "media", f"{channel_name}/video/$Time$")
+            for mpd_segmenttemplate in mpd_root.iterfind(f".//{{{self.MPD_NS}}}Representation[@mimeType='audio/mp4']/{{{self.MPD_NS}}}SegmentTemplate"):
+                mpd_segmenttemplate.set(
+                    "initialization", base_url + "/" + mpd_segmenttemplate.get("initialization"))
+                mpd_segmenttemplate.set(
+                    "media", f"{channel_name}/audio/$Time$")
             playlist = ET.tostring(
                 mpd_root, encoding='utf-8').decode('utf-8')
-            #print(playlist)
         else:
             raise Exception("Wrong channel name")
         return playlist
@@ -92,24 +95,21 @@ class Provider:
     def get_channel_segment(self, channel_name, type, time):
         mpd_url_cache_value = self._mpd_url_cache.get(channel_name)
         url = mpd_url_cache_value[1]
-        mpd_xml = self._opener.open(url).read()
-        mpd_xml_ns = "urn:mpeg:dash:schema:mpd:2011"
-        ET.register_namespace("", mpd_xml_ns)
-        mpd_root = ET.fromstring(mpd_xml)
         base_url = url.split("?")[0].rsplit("/", 1)[0] + "/"
-        for mpd_adaptationset in mpd_root.iterfind(f".//{{{mpd_xml_ns}}}AdaptationSet"):
-            for mpd_representation in mpd_adaptationset.findall(f"{{{mpd_xml_ns}}}Representation"):
-                if mpd_representation.get("mimeType") == "video/mp4" and (not mpd_representation.get("height") in ("480", "540")):    # FIXME
-                        mpd_adaptationset.remove(mpd_representation)
-        for mpd_segmenttemplate in mpd_root.iterfind(f".//{{{mpd_xml_ns}}}Representation[@mimeType='{type}/mp4']/{{{mpd_xml_ns}}}SegmentTemplate"):
-            for mpd_s in mpd_segmenttemplate.iterfind(f"{{{mpd_xml_ns}}}SegmentTimeline/{{{mpd_xml_ns}}}S"):
+        mpd_str = self._opener.open(url).read()
+        ET.register_namespace("", self.MPD_NS)
+        mpd_root = ET.fromstring(mpd_str)
+        self._limit_video_representation(mpd_root)
+        # FIXME pramborstv representation id
+        for mpd_segmenttemplate in mpd_root.iterfind(f".//{{{self.MPD_NS}}}Representation[@mimeType='{type}/mp4']/{{{self.MPD_NS}}}SegmentTemplate"):
+            for mpd_s in mpd_segmenttemplate.iterfind(f"{{{self.MPD_NS}}}SegmentTimeline/{{{self.MPD_NS}}}S"):
                 if int(mpd_s.get('t')) >= int(time):
-                    #print(base_url + mpd_segmenttemplate.get("media").replace("$Time$", mpd_s.get('t')))
-                    segment = self._opener.open(base_url + mpd_segmenttemplate.get("media").replace("$Time$", mpd_s.get('t'))).read()
+                    segment = self._opener.open(
+                        base_url + mpd_segmenttemplate.get("media").replace("$Time$", mpd_s.get('t'))).read()
                     return segment
 
     def get_kodi_props(self, channel_name):
-        if channel_name in self._mpd_channels:
+        if channel_name in self.MPD_CHANNELS:
             return (
                 "#KODIPROP:inputstream=inputstream.adaptive\n"
                 "#KODIPROP:inputstream.adaptive.manifest_type=mpd\n"
@@ -119,3 +119,9 @@ class Provider:
 
     def get_kodi_url_suffix(self):
         return "|user-agent=Mozilla"
+
+    def _limit_video_representation(self, mpd_root):
+        for mpd_adaptationset in mpd_root.iterfind(f".//{{{self.MPD_NS}}}AdaptationSet"):
+            for mpd_representation in mpd_adaptationset.findall(f"{{{self.MPD_NS}}}Representation"):
+                if mpd_representation.get("mimeType") == "video/mp4" and (not mpd_representation.get("height") in ("480", "540")):    # FIXME
+                    mpd_adaptationset.remove(mpd_representation)
